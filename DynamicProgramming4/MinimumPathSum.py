1ce9e07d7c990a7fb76d2f4159f5772aa2b86512
Koushik <55914356+koushik147@users.noreply.github.com>
2022-11-22 03:03:07 -0500

    Add files via upload

---
+++ DynamicProgramming4/MinimumPathSum.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def minPathSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        
+        m = len(grid) # length of grid
+        n = len(grid[0]) # length of grid of 0
+
+        for j in range(n-2,-1,-1):
+            grid[m-1][j] += grid[m-1][j+1] # run until the loop and add the grid of m-1th value
+
+        for i in range(m-2,-1,-1):
+            grid[i][n-1] += grid[i+1][n-1] # run until the loop and add the grid of n-1th value
+
+        for i in range (m-2,-1,-1):
+            for j in range(n-2,-1,-1): # run until the grid reaches last cell then add the minimum between the grid[i+1][j] and grid[i][j+1]
+                grid[i][j] += min(grid[i+1][j],grid[i][j+1])
+
+        return grid[0][0] # return the grid starting value
---
+++ DynamicProgramming4/UniquePaths.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        prevrow = [1] *n # creating the previous row with 1 as array with length of n
+
+        if m == 1: # if m is equal to 1 then return 1
+            return 1
+
+        for i in range(m-1):
+            currRow = [1]*n # creating the current row with value 1 with length of n
+            for j in range(n-2,-1,-1):
+                currRow[j] = currRow[j+1] + prevrow[j] # run until the loop by currRow  with currRow and prevRow
+
+            prevrow = currRow # assigning the current row to prev row
+
+        return currRow[0] # return the 0th value of current row 
